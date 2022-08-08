2ca6f6d5868adda4b77df1af2a7d7cdc40b16b8e
Adel <56457765+ad3ldev@users.noreply.github.com>
2022-08-08 21:04:42 +0200

    Minimum Viable Product

--- app/src/main/java/com/udacity/shoestore/ShoeDetailsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailsFragment.kt
@@ -1,18 +1,15 @@
 package com.udacity.shoestore
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.InverseMethod
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeDetailsBinding
-
 
 class ShoeDetailsFragment : Fragment() {
     lateinit var binding: FragmentShoeDetailsBinding
@@ -24,21 +21,57 @@ class ShoeDetailsFragment : Fragment() {
     ): View? {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_details, container, false)
-
-
         viewModel = ViewModelProvider(requireActivity())[ShoeViewModel::class.java]
         binding.viewModel = viewModel
-
-
         binding.addButton.setOnClickListener {
-            viewModel.addToShoeList()
-            findNavController().popBackStack()
+            if (validateSize()) {
+                viewModel.shoe.size = binding.addShoeSize.text.toString().toDouble()
+            }
+            if (validateName() && validateDetails() && validateCompany() && validateSize()) {
+                viewModel.addToShoeList()
+                findNavController().popBackStack()
+            } else {
+                val builder: AlertDialog.Builder? = activity?.let {
+                    AlertDialog.Builder(it)
+                }
+                builder?.setMessage(R.string.invalid_input)
+                    ?.setTitle("Invalid Input")
+                val dialog: AlertDialog? = builder?.create()
+                dialog?.show()
+            }
         }
-        binding.cancelButton.setOnClickListener{
+        binding.cancelButton.setOnClickListener {
             viewModel.clearShoe()
             findNavController().popBackStack()
         }
-
         return binding.root
+    }
+
+    private fun validateSize(): Boolean {
+        if (binding.addShoeSize.text.isBlank()) {
+            return false
+        }
+        return true
+    }
+
+    private fun validateDetails(): Boolean {
+        if (binding.addShoeDetails.text.isBlank()) {
+            return false
+        }
+        return true
+    }
+
+    private fun validateCompany(): Boolean {
+        if (binding.addShoeCompany.text.isBlank()) {
+            return false
+        }
+        return true
+    }
+
+    private fun validateName(): Boolean {
+        if (binding.addShoeName.text.isBlank()) {
+            return false
+        }
+        return true
     }
 }
